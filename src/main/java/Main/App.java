ae926460bfbc1f9b22f610f378cd44e02401b813
luis miguel <luismrocha132@gmail.com>
2022-04-13 13:51:11 -0500

    inicio proyecto

---
+++ src/main/java/Main/App.java
@@ -0,0 +1,7 @@
+package Main;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("hola mundo");
+    }
+}
---
+++ src/main/java/utilidades/Fecha.java
@@ -0,0 +1,5 @@
+package utilidades;
+
+public interface Fecha {
+    public static final String FORMATO_FECHA = "dd/MM/yyyy";
+}
